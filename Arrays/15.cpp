92511a963e4384d64176b198d8e0be8d4385c8a8
Esha <204330677+bugbrew@users.noreply.github.com>
2026-01-20 14:46:26 +0530

    Solved binary search-type problems

---
+++ Arrays/15.cpp
@@ -0,0 +1,50 @@
+// 222. Count Complete Tree Nodes
+#include <iostream>
+using namespace std;
+
+// Definition for a binary tree node
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x)
+    {
+        val = x;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+// Function to count nodes
+int countNodes(TreeNode *root)
+{
+    if (root == nullptr)
+        return 0;
+
+    return 1 + countNodes(root->left) + countNodes(root->right);
+}
+
+int main()
+{
+    /*
+        Creating this tree:
+
+                1
+               / \
+              2   3
+             / \
+            4   5
+    */
+
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+
+    cout << "Total nodes: " << countNodes(root) << endl;
+
+    return 0;
+}
