eb3d1d2d4af9eaa979f187d650d940f3dadcf863
Esha <204330677+bugbrew@users.noreply.github.com>
2026-01-30 15:47:04 +0530

    Added Kadane's algorithm problems

---
+++ Kadane's/01_MaxSubarray.cpp
@@ -0,0 +1,23 @@
+// 53. Maximum Subarray
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int maxSubArray(vector<int> &nums) {
+    int currSum = nums[0];
+    int maxSum = 0;
+
+    for(int i = 1; i< nums.size(); i++){
+        currSum = max(nums[i], currSum + nums[i]);
+        maxSum = max(maxSum, currSum);
+    }
+    return maxSum;
+};
+
+int main(){
+    vector<int> nums = {-2,1,-3,4,-1,2,1,-5,4};
+
+    int result = maxSubArray(nums);
+    cout << result;
+    return 0;
+}
