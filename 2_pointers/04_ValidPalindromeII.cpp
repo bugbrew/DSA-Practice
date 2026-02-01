5b5c8628ead8fdd7e5cda917e99d4500b4b5e7b8
Esha <204330677+bugbrew@users.noreply.github.com>
2026-02-01 14:13:38 +0530

    Solved problem Valid Palindrome II using 2 pointers approach with greedy 1 time skip pattern

---
+++ 2_pointers/04_ValidPalindromeII.cpp
@@ -0,0 +1,34 @@
+// 680. Valid Palindrome II
+#include<iostream>
+using namespace std;
+
+bool isPalindrome(string s, int left, int right){
+    while(left < right){
+        if(s[left] != s[right]){
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+};
+
+bool validPalindrome(string s){
+    int left = 0;
+    int right = s.length() - 1;
+
+    while(left < right){
+        if (s[left] != s[right]){
+            return isPalindrome(s, left+ 1, right) || isPalindrome(s, left, right - 1);
+        }
+        left++;
+        right--;
+    }
+    return true;
+};
+int main(){
+    string s;
+    cin >> s;
+    cout << validPalindrome(s) << " ";
+    return 0;
+}
