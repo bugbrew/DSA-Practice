255af170e4631dafd16b48aa406a5dec207898c2
Esha <204330677+bugbrew@users.noreply.github.com>
2026-02-10 20:53:13 +0530

    Solved problem Longest Balanced Subarray I using unordered set

---
+++ Dynamic_Programming/dsa07_LongestBalancedSubarray1.cpp
@@ -0,0 +1,35 @@
+// 3719. Longest Balanced Subarray I
+#include<iostream>
+#include<vector>
+#include <unordered_set>
+using namespace std;
+
+int longestBalance(vector<int>& nums){
+    int maxLen = 0;
+    for (int i = 0; i < nums.size(); i++){
+        unordered_set<int> evenSet;
+        unordered_set<int> oddSet;
+
+        for (int j = i; j < nums.size(); j++){
+            if (nums[j] % 2 == 0)
+                evenSet.insert(nums[j]);
+            else
+                oddSet.insert(nums[j]);
+
+            if (evenSet.size() == oddSet.size()){
+                maxLen = max(maxLen, j - i + 1);
+            }
+        }
+    }
+    return maxLen;
+};
+
+int main(){
+
+    vector<int> nums = {1, 2, 3, 2};
+
+    cout << longestBalance(nums) << endl;
+
+
+    return 0;
+}
