1b755ea8270402f367be0b3d25f1d98e794f8721
Esha <204330677+bugbrew@users.noreply.github.com>
2026-02-11 14:01:36 +0530

    Solved Linked List Cycle II using Floyd's Cycle Detection Algorithm

---
+++ 2_pointers/09_LinkedListCycleII.cpp
@@ -0,0 +1,51 @@
+// 142. Linked List Cycle II
+#include<iostream>
+using namespace std;
+
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode(int x):val(x), next(NULL){}
+};
+
+ListNode *detectCycle(ListNode *head){
+    if(head == NULL){
+        return NULL;
+    }
+    ListNode *slow = head;
+    ListNode *fast = head;
+    while(fast != NULL && fast -> next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if(slow == fast){
+            slow = head;
+
+            while(slow != fast){
+                slow = slow->next;
+                fast = fast->next;
+            }
+            return slow;
+        }
+    }
+    return NULL;
+};
+int main(){
+    ListNode *head = new ListNode(3);
+    ListNode *second = new ListNode(2);
+    ListNode *third = new ListNode(0);
+    ListNode *fourth = new ListNode(4);
+
+    head->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = second;
+
+    ListNode* ans = detectCycle(head);
+    if(head != NULL){
+        cout << ans->val << " ";
+    }
+
+
+    return 0;
+}
