87dbd122f3b6c5a04d4953e9e8e5d701860f0067
Esha <204330677+bugbrew@users.noreply.github.com>
2026-03-17 17:10:02 +0530

    Solved a math related problem

---
+++ Random_Ques/dsa54_ExcelSheetColumnNum.cpp
@@ -0,0 +1,21 @@
+// 171. Excel Sheet Column Number
+#include <iostream>
+using namespace std;
+
+int titleToNumber(string columnTitle)
+{
+    int result = 0;
+
+    for (char c : columnTitle)
+    {
+        result = result * 26 + (c - 'A' + 1);
+    }
+
+    return result;
+}
+
+int main()
+{
+    cout << titleToNumber("AB") << endl; // 28
+    return 0;
+}
