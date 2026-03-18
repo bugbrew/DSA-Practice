fd6e2c981e99b3995887e88ce436fb36de053237
Esha <204330677+bugbrew@users.noreply.github.com>
2026-03-18 13:26:08 +0530

    Added some changes

--- Sorting/2_LongestHarmoniousSubsequence.cpp
+++ Sorting/2_LongestHarmoniousSubsequence.cpp
@@ -1,1 +1,14 @@
 // 594. Longest Harmonious Subsequence
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int findLHS(vector<int> &nums){
+
+};
+
+int main(){
+
+    return 0;
+}
