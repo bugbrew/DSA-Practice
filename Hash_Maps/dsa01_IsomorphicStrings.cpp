07b27e00fb9bcefd3e69d440971ae1740a0ee691
Esha <204330677+bugbrew@users.noreply.github.com>
2026-03-19 14:28:48 +0530

    Created a new folder for hash maps related questions

---
+++ Hash_Maps/dsa01_IsomorphicStrings.cpp
@@ -0,0 +1,47 @@
+// 205. Isomorphic Strings
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isIsomorphic(string s, string t)
+    {
+        if (s.size() != t.size())
+            return false;
+
+        unordered_map<char, char> mapST;
+        unordered_map<char, char> mapTS;
+
+        for (int i = 0; i < s.size(); i++)
+        {
+            char c1 = s[i];
+            char c2 = t[i];
+
+            // check s -> t
+            if (mapST.count(c1))
+            {
+                if (mapST[c1] != c2)
+                    return false;
+            }
+            else
+            {
+                mapST[c1] = c2;
+            }
+
+            // check t -> s
+            if (mapTS.count(c2))
+            {
+                if (mapTS[c2] != c1)
+                    return false;
+            }
+            else
+            {
+                mapTS[c2] = c1;
+            }
+        }
+
+        return true;
+    }
+};
