d78a3643dc8c4a1da2f0a9e02865fd60410d4c93
Esha <204330677+bugbrew@users.noreply.github.com>
2026-05-13 19:39:44 +0530

    Solved a problem based on sorting

---
+++ Sorting/5_SquaresOfSortedArray.cpp
@@ -0,0 +1,40 @@
+// 977. Squares of a Sorted Array
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> sortedSquares(vector<int> &nums)
+    {
+        vector<int> temp = nums;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            temp[i] = nums[i] * nums[i];
+        }
+
+        sort(temp.begin(), temp.end());
+
+        return temp;
+    }
+};
+
+int main()
+{
+    Solution sol;
+
+    vector<int> nums = {-4, -1, 0, 3, 10};
+
+    vector<int> result = sol.sortedSquares(nums);
+
+    cout << "Sorted Squares: ";
+    for (int num : result)
+    {
+        cout << num << " ";
+    }
+
+    return 0;
+}
