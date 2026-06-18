2906946224c7731ab5f46c1ef906a8a2116fd9b0
Esha <204330677+bugbrew@users.noreply.github.com>
2026-06-18 17:51:04 +0530

    SOlved a problem using dynamic programming

---
+++ Dynamic_Programming/dsa10_MaxRepeatSubstring.cpp
@@ -0,0 +1,34 @@
+// 1668. Maximum Repeating Substring
+#include <iostream>
+#include <string>
+using namespace std;
+
+int maxRepeating(string sequence, string word)
+{
+    int k = 0;
+    string temp = word;
+
+    while (sequence.find(temp) != string::npos)
+    {
+        k++;
+        temp += word;
+    }
+
+    return k;
+}
+
+int main()
+{
+    string sequence, word;
+
+    cout << "Enter sequence: ";
+    cin >> sequence;
+
+    cout << "Enter word: ";
+    cin >> word;
+
+    cout << "Maximum k-repeating value = "
+         << maxRepeating(sequence, word);
+
+    return 0;
+}
